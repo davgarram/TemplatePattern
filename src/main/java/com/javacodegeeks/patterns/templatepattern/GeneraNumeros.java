66daa6cb19bbb8eb2664b4028187ed588184588f
David García <davgarram@alum.us.es>
2021-02-09 13:40:59 +0100

    Otra version arreglando detalles

--- src/main/java/com/javacodegeeks/patterns/templatepattern/GeneraNumeros.java
+++ src/main/java/com/javacodegeeks/patterns/templatepattern/GeneraNumeros.java
@@ -1,6 +1,7 @@
 package com.javacodegeeks.patterns.templatepattern;
 
 public class GeneraNumeros {
+	private float a=0;
 	public static float generarFloat() 
 	{
 		return 5; //Siempre devuelve el mismo numero. En un caso real devolvería un número aleatorio o algo así
@@ -11,6 +12,15 @@ public class GeneraNumeros {
 	}
 	public static float copiarFloat(GeneraNumeros g) 
 	{
-		return g.generarFloat(); //Debería acceder de forma estatica, pero así puedo probarlo con Mockito
+		return g.getFloat(); 
 	}
+	private float getFloat() {
+		// TODO Auto-generated method stub
+		return a;
+	}
+	private void setFloat(float b) 
+	{
+		a=b;
+	}
+	
 }
